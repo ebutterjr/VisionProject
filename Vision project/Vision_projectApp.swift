9b6e223e7d9e7979d4cfeb8e90c82093ba95856d
Easton <ebutterjr@gmail.com>
2023-06-07 18:58:18 -0600

    Initial commit

---
+++ Vision project/Vision_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vision_projectApp.swift
+//  Vision project
+//
+//  Created by Easton Butterfield on 6/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Vision_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
